afedc823fb6fe7a521b70845c322dcbd3e2af717
Tushar <kmrtushar01@gmail.com>
2024-04-01 20:42:57 +0530

    adding first half jsx code of form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,159 @@
+import { useState } from "react";
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: "", lastName: "", email: "", country: "",
+    streetAddress: "", city: "", state: "", zip: ""
+  });
+
+  function changeHandler(event) {
+    setFormData((pre) => {
+      return {
+        ...pre,
+        [event.target.name]: event.target.value
+      }
+    }
+
+    )
+  }
+
   return (
     <div className="App">
-      
+      <form>
+        <div className="basic-info">
+          <div>
+            <label htmlFor="fname">First Name: </label>
+
+            <input className=" border-black"
+              type="text"
+              onChange={changeHandler}
+              placeholder="first Name"
+              name="fname"
+              id="fname"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="lastName">Last Name: </label>
+
+            <input className=" border-black"
+              type="text"
+              onChange={changeHandler}
+              placeholder="Last Name"
+              name="lastName"
+              id="lastName"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email Address: </label>
+
+            <input className=" border-black"
+              type="email"
+              onChange={changeHandler}
+              placeholder="xyz@gmail.com"
+              name="email"
+              id="email"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="country">Country : </label>
+            <select
+              name="country"
+              id="country"
+              onChange={changeHandler}
+            >
+              <option value="">Select</option>
+              <option value="India">India</option>
+              <option value="Australia">Australia</option>
+              <option value="Japan">Japan</option>
+              <option value="US">US</option>
+              <option value="Russia">Russia</option>
+            </select>
+
+          </div>
+
+
+          <div>
+            <label htmlFor="streetAddress">Street Address: </label>
+
+            <input className=" border-black"
+              type="text"
+              onChange={changeHandler}
+              placeholder="1234 Main street "
+              name="streetAddress"
+              id="streetAddress"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="city">City: </label>
+
+            <input className=" border-black"
+              type="text"
+              onChange={changeHandler}
+              placeholder="Meerut"
+              name="city"
+              id="city"
+            />
+          </div>
+
+
+          <div>
+            <label htmlFor="state">State: </label>
+            <select
+              name="state"
+              id="state"
+              onChange={changeHandler}
+            >
+                <option value="">Select</option>
+                <option value="Andhra Pradesh">Andhra Pradesh</option>
+                <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+                <option value="Assam">Assam</option>
+                <option value="Bihar">Bihar</option>
+                <option value="Chhattisgarh">Chhattisgarh</option>
+                <option value="Goa">Goa</option>
+                <option value="Gujarat">Gujarat</option>
+                <option value="Haryana">Haryana</option>
+                <option value="Himachal Pradesh">Himachal Pradesh</option>
+                <option value="Jharkhand">Jharkhand</option>
+                <option value="Karnataka">Karnataka</option>
+                <option value="Kerala">Kerala</option>
+                <option value="Madhya Pradesh">Madhya Pradesh</option>
+                <option value="Maharashtra">Maharashtra</option>
+                <option value="Manipur">Manipur</option>
+                <option value="Meghalaya">Meghalaya</option>
+                <option value="Mizoram">Mizoram</option>
+                <option value="Nagaland">Nagaland</option>
+                <option value="Odisha">Odisha</option>
+                <option value="Punjab">Punjab</option>
+                <option value="Rajasthan">Rajasthan</option>
+                <option value="Sikkim">Sikkim</option>
+                <option value="Tamil Nadu">Tamil Nadu</option>
+                <option value="Telangana">Telangana</option>
+                <option value="Tripura">Tripura</option>
+                <option value="Uttar Pradesh">Uttar Pradesh</option>
+                <option value="Uttarakhand">Uttarakhand</option>
+                <option value="West Bengal">West Bengal</option>
+            </select>
+          </div>
+
+
+          <div>
+            <label htmlFor="zip">ZIP / postal code: </label>
+
+            <input className=" border-black"
+              type="text"
+              onChange={changeHandler}
+              placeholder="245206"
+              name="zip"
+              id="zip"
+            />
+          </div>
+        </div>
+
+      </form>
     </div>
   );
 }
